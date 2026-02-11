6b3877f2f7ea03965945cc680a3200078deca1a7
Karthik Ramadugu <karthiksaidham2001@gmail.com>
2026-02-10 23:00:46 -0500

    feat: add calculateAge utility function for accurate DOB-based age calculation

---
+++ src/utils/calculateAge.js
@@ -0,0 +1,17 @@
+/**
+ * Calculate age from date of birth
+ * @param {Date|string} dob - Date of birth
+ * @returns {number} Age in years
+ */
+function calculateAge(dob) {
+  const birthDate = new Date(dob);
+  const today = new Date();
+  let age = today.getFullYear() - birthDate.getFullYear();
+  const monthDiff = today.getMonth() - birthDate.getMonth();
+  if (monthDiff < 0 || (monthDiff === 0 && today.getDate() < birthDate.getDate())) {
+    age--;
+  }
+  return age;
+}
+
+module.exports = calculateAge;
